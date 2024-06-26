e0db20358514f328efb5a351216b32f72b92b8d4
DKKolev <121520125+mk6300@users.noreply.github.com>
2024-06-26 09:55:43 +0300

    Coding version

--- src/main/java/com/plannerapp/model/dto/RegisterDTO.java
+++ src/main/java/com/plannerapp/model/dto/RegisterDTO.java
@@ -8,7 +8,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class RegisterDTO {
-    private Long id;
 
     @UniqueUsername
     @Size(min = 3, max = 20, message = "Username length must be between 3 and 20 characters!")
@@ -31,13 +30,6 @@ public class RegisterDTO {
     public RegisterDTO() {
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getUsername() {
         return username;
